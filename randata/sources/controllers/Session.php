07509004a88d47dd3c73fc1d361777160829bca7
Duffy <web.duckdodgers@gmail.com>
2024-05-31 09:23:23 +0200

    Reset the .gitgnore

--- randata/sources/controllers/Session.php
+++ randata/sources/controllers/Session.php
@@ -7,21 +7,6 @@ use controllers\Subscribe;
 
 class Session
 {
-    public function to_login()
-    {
-        session_start();
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $email = $_POST['email'];
-            $password = $_POST['password'];
-
-            $login = new Login();
-            $result = $login->authenticate($email, $password);
-
-            echo $result;
-        }
-    }
-
     public function to_subscribe()
     {
         session_start();
@@ -35,6 +20,21 @@ class Session
 
             $newUser = new Subscribe();
             $result = $newUser->register($firstName, $lastName, $nickName, $email, $password);
+
+            echo $result;
+        }
+    }
+
+    public function to_login()
+    {
+        session_start();
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            $login = new Login();
+            $result = $login->authenticate($email, $password);
 
             echo $result;
         }
